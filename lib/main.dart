431e169052dab4010e166a0e6caa917bdcd7dc07
Dhruvjayal <dhruvjayal.work@gmail.com>
2022-09-18 01:08:36 +0530

    day 17 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 import 'package:flutter_application_1/widgets/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
